6e4c129d42b5d978a8f130568614d59a9d38d4fe
Grant Uy <grant.uy@utexas.edu>
2016-09-29 17:16:36 -0500

    Implement additional unit tests

--- MainTest.java
+++ MainTest.java
@@ -81,4 +81,117 @@ public class MainTest {
 		}
 		System.out.println("pass");
 	}
-)
+	
+	@Test
+	public void NoLadder_DFS() {
+		Main.initialize();
+		ArrayList<String> ladder = Main.getWordLadderDFS("HAPPY", "IDIOT");
+		Main.printLadder(ladder);
+		if(ladder.size() > 0){
+			fail("Incorrect ladder");
+		}
+		for (int i = 0; i < ladder.size(); i++){
+			for (int j = 0; j < ladder.size(); j++){
+				if((ladder.get(i) == ladder.get(j)) && (i != j)){
+					fail("Duplicate words in ladder");
+					return;
+				}
+			}
+		}
+		System.out.println("pass");
+	}
+	
+	@Test
+	public void NoLadder_BFS() {
+		Main.initialize();
+		ArrayList<String> ladder = Main.getWordLadderBFS("HAPPY", "IDIOT");
+		Main.printLadder(ladder);
+		if(ladder.size() > 0){
+			fail("Incorrect ladder");
+		}
+		for (int i = 0; i < ladder.size(); i++){
+			for (int j = 0; j < ladder.size(); j++){
+				if((ladder.get(i) == ladder.get(j)) && (i != j)){
+					fail("Duplicate words in ladder");
+					return;
+				}
+			}
+		}
+		System.out.println("pass");
+	}
+	
+	@Test
+	public void LongFlipped_BFS() {
+		Main.initialize();
+		ArrayList<String> ladder = Main.getWordLadderBFS("HOUSE", "DRILL");
+		if(!(ladder.get(0).equals("HOUSE")) || !(ladder.get(ladder.size() - 1)).equals("DRILL")){
+			fail("Incorrect ladder");
+		}
+		for (int i = 0; i < ladder.size(); i++){
+			for (int j = 0; j < ladder.size(); j++){
+				if((ladder.get(i) == ladder.get(j)) && (i != j)){
+					fail("Duplicate words in ladder");
+					return;
+				}
+			}
+		}
+		System.out.println("pass");
+	}
+	
+	@Test
+	public void LongFlipped_DFS() {
+		Main.initialize();
+		ArrayList<String> ladder = Main.getWordLadderDFS("HOUSE", "DRILL");
+		if(!(ladder.get(0).equals("HOUSE")) || !(ladder.get(ladder.size() - 1)).equals("DRILL")){
+			fail("Incorrect ladder");
+		}
+		Main.printLadder(ladder);
+		for (int i = 0; i < ladder.size(); i++){
+			for (int j = 0; j < ladder.size(); j++){
+				if((ladder.get(i) == ladder.get(j)) && (i != j)){
+					fail("Duplicate words in ladder");
+					return;
+				}
+			}
+		}
+		System.out.println("pass");
+	}
+	
+	@Test
+	public void NoLadderFlipped_DFS() {
+		Main.initialize();
+		ArrayList<String> ladder = Main.getWordLadderDFS("IDIOT", "HAPPY");
+		Main.printLadder(ladder);
+		if(ladder.size() > 0){
+			fail("Incorrect ladder");
+		}
+		for (int i = 0; i < ladder.size(); i++){
+			for (int j = 0; j < ladder.size(); j++){
+				if((ladder.get(i) == ladder.get(j)) && (i != j)){
+					fail("Duplicate words in ladder");
+					return;
+				}
+			}
+		}
+		System.out.println("pass");
+	}
+	
+	@Test
+	public void NoLadderFlipped_BFS() {
+		Main.initialize();
+		ArrayList<String> ladder = Main.getWordLadderBFS("IDIOT", "HAPPY");
+		Main.printLadder(ladder);
+		if(ladder.size() > 0){
+			fail("Incorrect ladder");
+		}
+		for (int i = 0; i < ladder.size(); i++){
+			for (int j = 0; j < ladder.size(); j++){
+				if((ladder.get(i) == ladder.get(j)) && (i != j)){
+					fail("Duplicate words in ladder");
+					return;
+				}
+			}
+		}
+		System.out.println("pass");
+	}
+}
